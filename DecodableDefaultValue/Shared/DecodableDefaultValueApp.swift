5aa961966f3ccccffb094361dfab68a36dc5a119
Tayathorn 'Pan' Panuwattanawong <p.tayathorn@gmail.com>
2022-01-31 13:50:49 +0700

    feat: initial project

---
+++ DecodableDefaultValue/Shared/DecodableDefaultValueApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecodableDefaultValueApp.swift
+//  Shared
+//
+//  Created by tayathorn on 31/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct DecodableDefaultValueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
